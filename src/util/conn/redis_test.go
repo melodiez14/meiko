ba811271c738ec3f090bcd71be87c6d86c8880a6
Risal Falah <risal.falah@tokopedia.com>
2017-05-09 22:00:59 +0700

    add redigo mock

--- src/util/conn/redis_test.go
+++ src/util/conn/redis_test.go
@@ -1,1 +1,61 @@
 package conn
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/rafaeljusto/redigomock"
+)
+
+func initRedisMock() *redigomock.Conn {
+	conn := redigomock.NewConn()
+	Redis = &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 10 * time.Second,
+		Dial:        func() (redis.Conn, error) { return conn, nil },
+	}
+	return conn
+}
+
+func TestInitRedisMock(t *testing.T) {
+	cases := []struct {
+		command        string
+		key            string
+		expectedResult string
+		expectedErr1   error
+		expectedErr2   error
+	}{
+		{
+			command:        "GET",
+			key:            "test",
+			expectedResult: "success",
+			expectedErr1:   nil,
+			expectedErr2:   nil,
+		},
+	}
+
+	modConn := initRedisMock()
+
+	for _, val := range cases {
+		cmd := modConn.Command(val.command, val.key).Expect(val.expectedResult)
+
+		r, err := redis.String(modConn.Do(val.command, val.key))
+		if !reflect.DeepEqual(err, val.expectedErr1) {
+			t.Errorf("expect: %v got: %v", val.expectedErr1, err)
+			t.FailNow()
+		}
+
+		if r != val.expectedResult {
+			t.Errorf("expect %v got %v", val.expectedResult, r)
+			t.FailNow()
+		}
+
+		if modConn.Stats(cmd) != 1 {
+			t.Errorf("Command was not used")
+			t.FailNow()
+		}
+	}
+
+}
