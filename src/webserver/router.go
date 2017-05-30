4f7cdd9c3b5dc7aea98c6063d1a257379625f0e5
klikkanan <asepnur.isk@gmail.com>
2017-05-30 22:18:12 +0700

    add db schema

--- src/webserver/router.go
+++ src/webserver/router.go
@@ -4,18 +4,9 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/melodiez14/meiko/src/util/auth"
 	"github.com/melodiez14/meiko/src/webserver/handler"
-	"github.com/melodiez14/meiko/src/webserver/handler/assignment"
-	"github.com/melodiez14/meiko/src/webserver/handler/attendance"
-	"github.com/melodiez14/meiko/src/webserver/handler/course"
-	"github.com/melodiez14/meiko/src/webserver/handler/notification"
 )
 
 // Load returns all routing of this server
 func loadRouter(r *httprouter.Router) {
 	r.GET("/api/v1/hellomeiko", auth.OptionalAuthorize(handler.HelloMeiko))
-	r.GET("/api/v1/course/summary", auth.MustAuthorize(course.GetSummaryHandler))
-	r.GET("/api/v1/assignment/incomplete", auth.MustAuthorize(assignment.GetIncompleteHandler))
-	r.GET("/api/v1/assignment/summary", auth.MustAuthorize(assignment.GetIncompleteHandler))
-	r.GET("/api/v1/attendance/summary", auth.MustAuthorize(attendance.GetSummaryHandler))
-	r.GET("/api/v1/notification", auth.MustAuthorize(notification.GetHandler))
 }
